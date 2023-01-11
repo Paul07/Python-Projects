07e20cf31d29c0177a5793b8f2f4c15dcc1946eb
Paul Brandenburger <paulbrandenburger1@gmail.com>
2023-01-11 09:40:02 -0500

    Update user_submit.py
    Cleaning it up

--- user_submit.py
+++ user_submit.py
@@ -1,16 +1,16 @@
 #initial class of 'shoe'
 class shoe:
-    brand = ''
-    color = ''
-    size = ''
-    cost = int#I am not sure that this is the right type,I did see that there is
+    brand = 'Puma'
+    color = 'black'
+    size = '13'
+    cost = 0#I am not sure that this is the right type,I did see that there is
               #a money module, just wasn't trying to get that deep yet.
 
 #first instance of a child class
 class boot(shoe):
     waterproof = True
-    style = ''
-    material = ''
+    style = 'Work'
+    material = 'Leather'
     
 #second instance of a child class
 class flop(shoe):
